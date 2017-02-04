6d272924e15ea311487a032c90d0a2df58310b98
Matthew Zimmer <M16r01@gmail.com>
2017-02-04 15:41:03 -0500

    classroom notes.

--- pipeline_ops.py
+++ pipeline_ops.py
@@ -7,20 +7,26 @@
 
       1. Declare a constructor with inputs necessary to perform the operation in `#perform`.
 
-      2. Implement `#output` which returns the result of performing the operation in `#perform`.
+      2. Implement `#perform`
 
-      3. Implement `#perform` ensuring that the last line is `return self`.
-         This provides support to perform the op and immediately assign `#output`
-         to local variables.
+          * This method must return `self`. This provides support to perform the op and 
+            immediately assign the call to `#output` to local variables.
+
+          * Declared your op's output by calling `#_apply_output` once you've performed your operation.
 
 """
 
-class PipelineOp:
-	def perform(self):
-		raise NotImplementedError
 
-	def output(self):
-		"""
-		Returns the result from performing this operation.
-		"""
-		raise NotImplementedError
+class PipelineOp:
+  def __init__(self):
+    self.__output = None
+
+  def perform(self):
+    raise NotImplementedError
+
+  def output(self):
+    return self.__output
+
+  def _apply_output(self, value):
+    self.__output = value
+    return self
--- vehicle_detection_pipeline_operations.py
+++ vehicle_detection_pipeline_operations.py
@@ -6,3 +6,21 @@ import pickle
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
+from pipeline_ops import PipelineOp
+
+class DrawBoxesOp(PipelineOp):
+    def __init__(self, img, bboxes, color=(0, 140, 255), thickness=5):
+        PipelineOp.__init__(self)
+        self.__img = img
+        self.__bboxes = bboxes
+        self.__color = color
+        self.__thickness = thickness
+    
+    def perform(self):
+	    # Make a copy of the image
+	    output = np.copy(self.__img)
+	    # Iterate through the bounding boxes
+	    for pt1, pt2 in self.__bboxes:
+	        # Draw a rectangle given bbox coordinates
+	        cv2.rectangle(output, pt1, pt2, self.__color, thickness=self.__thickness)
+        return self._apply_output(output)
